400a9629347649b2972289b747167d398def414c
David <ddebrah@gmail.com>
2025-07-29 17:58:54 -0600

    Not Relevant - delete later

---
+++ src/vite-env.d.ts
@@ -0,0 +1,25 @@
+import { defineConfig } from 'vite';
+
+interface ImportMetaEnv {
+  readonly VITE_FIREBASE_API_KEY: string;
+  readonly VITE_FIREBASE_AUTH_DOMAIN: string;
+  readonly VITE_FIREBASE_PROJECT_ID: string;
+  readonly VITE_FIREBASE_STORAGE_BUCKET: string;
+  readonly VITE_FIREBASE_MESSAGING_SENDER_ID: string;
+  readonly VITE_FIREBASE_APP_ID: string;
+  // add other .env vars if needed
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv;
+}
+
+export default defineConfig({
+  optimizeDeps: {
+    esbuildOptions: {
+      define: {
+        global: 'globalThis',
+      },
+    },
+  },
+});
