a57d2a2f6492f917ebb39a2b50b85a62e6d60860
David <ddebrah@gmail.com>
2025-07-30 15:10:39 -0600

    Updated Pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,23 @@
 import React from 'react';
 import { createBrowserRouter } from 'react-router-dom';
 import MainPage from "./Pages/Home/main";
-// import Login from "./pages/login";
-// import ClockinPage from "./pages/clockin";
-// import AttendanceHistory from "./pages/attendancehistory";
-// import AdminDashboard from "./pages/admindashboard";
+import NutritionPage from "./Pages/Nutrition/nutrition";
+import RecipePage from "./Pages/Recipes/recipes";
+
 
 const App = createBrowserRouter([
   {
     path: '/',
-    // element: <Admindashboard />,
-    // element: <ClockinPage />,
-    // element: <Login />,
     element: <MainPage />,
   },
-  // {
-  //   path: '/clockin',
-  //   element: <ClockinPage />,
-  // },
-  // {
-  //   path: '/history',
-  //   element: <AttendanceHistory />,
-  // },
-  // {
-  //   path: '/admin',
-  //   element: <AdminDashboard />
-  // }
+  {
+    path: '/nutrition',
+    element: <NutritionPage />,
+  },
+  {
+    path: '/recipes',
+    element: <RecipePage />,
+  },
 ]);
 
 export default App;
