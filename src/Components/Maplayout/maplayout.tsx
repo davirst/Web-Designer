5b26fbb0f5d657fbaeac83915ee209580af750b3
David <ddebrah@gmail.com>
2025-07-30 10:41:45 -0600

    Map layout for hidden items

---
+++ src/Components/Maplayout/maplayout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+function Maplayout() {
+    return (
+        <>
+            <map name = "tablet_area">
+                <area shape="Poly" alt="Bacon" href="https://en.wikipedia.org/wiki/Bacon" coords="355,320,344,299,335,285,350,272,363,268,381,265,393,272,415,253,429,254,442,246,450,247,463,230,474,240,484,252,476,268,468,284" />
+                <area shape="Poly" alt="Egg" href="https://en.wikipedia.org/wiki/Fried_egg" coords="292,107,279,94,275,79,285,71,295,63,309,57,324,38,342,34,360,36,376,43,386,45,403,45,419,52,433,58,440,70,440,81,436,90,380,104"/>
+                <area shape="Poly" alt="sausage" href="https://en.wikipedia.org/wiki/Sausage" coords="104,237,77,262,57,283,38,292,37,307,31,312,27,305,22,298,31,287,27,269,34,249,47,228,63,213,80,200,103,192,124,187,146,189,159,193,172,202,181,193,182,205" />
+            </map>
+
+            <figcaption></figcaption>
+        </>
+    )
+}
+
+export default Maplayout;
