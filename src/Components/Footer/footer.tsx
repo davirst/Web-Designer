c9330fa6ef72fc1523f7d7fb279a8b8e61158cdf
David <ddebrah@gmail.com>
2025-07-30 15:11:17 -0600

    Footer updated

--- src/Components/Footer/footer.tsx
+++ src/Components/Footer/footer.tsx
@@ -2,7 +2,10 @@ import React from "react";
 
 function Footer() {
     return (
-        <div className="table-footer-group"> Last modified: April 14th, 2020. Author: Davir </div>
+        <div className="text-center pt-4">
+            <div>Original Date: April 14th, 2020 & Last modified: July 30th, 2025.</div>
+            <div>Author: Davir</div>
+        </div>
     )
 }
 
