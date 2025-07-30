07ed665094b7d13ae282866b4263cb64387a78a7
David <ddebrah@gmail.com>
2025-07-30 15:11:06 -0600

    Three Menu Buttons

--- src/Components/Buttons/buttons.tsx
+++ src/Components/Buttons/buttons.tsx
@@ -1,12 +1,24 @@
 import React from "react";
 import {Button} from "@mui/material";
+import { useNavigate } from 'react-router-dom';
 
 function MenuButton() {
+    const navigate = useNavigate();
+
+    const goToHomePage = () => {
+        navigate('/');
+    };
+    const goToRecipePage = () => {
+        navigate('/recipes');
+    };
+    const goToNutritionPage = () => {
+        navigate('/nutrition');
+    };
     return (
-        <div className="flex gap-1">
-            <Button variant="outlined">Home</Button>
-            <Button variant="outlined">Recipes</Button>
-            <Button variant="outlined">Nutrition</Button>
+        <div className="flex gap-1 justify-end p-2 border-b-2 ">
+            <Button variant="outlined" onClick={goToHomePage}>Home</Button>
+            <Button variant="outlined" onClick={goToRecipePage}>Recipes</Button>
+            <Button variant="outlined" onClick={goToNutritionPage}>Nutrition</Button>
         </div>
     )
 }
