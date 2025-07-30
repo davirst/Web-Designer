9d85daad652c8fcc879bc7bc2b1fac3289a0c95e
David <ddebrah@gmail.com>
2025-07-30 10:37:40 -0600

    Footer for every page

---
+++ src/Components/Footer/footer.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+function Footer() {
+    return (
+        <div className="table-footer-group"> Last modified: April 14th, 2020. Author: Davir </div>
+    )
+}
+
+export default Footer;
