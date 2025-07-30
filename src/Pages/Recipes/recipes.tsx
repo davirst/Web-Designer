cd3c4da2725cc27fbc6e46afc3b4ecde7031e960
David <ddebrah@gmail.com>
2025-07-30 15:12:58 -0600

    recipe page

---
+++ src/Pages/Recipes/recipes.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Footer from "../../Components/Footer/footer";
+import MenuButton from "../../Components/Buttons/buttons";
+import Header from "../../Components/Header/header";
+
+function RecipePage() {
+    return (
+        <>
+            <MenuButton />
+            <Header />
+            <Footer />
+        </>
+    )
+}
+
+export default RecipePage;
