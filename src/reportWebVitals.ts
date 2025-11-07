4d31105beb1aee664e40a00a282f15f1eee0137c
David <ddebrah@gmail.com>
2025-11-07 14:48:31 -0700

    modified with getLCP removed

--- src/reportWebVitals.ts
+++ src/reportWebVitals.ts
@@ -1,15 +1,13 @@
-import { ReportHandler } from 'web-vitals';
+import { onCLS,onLCP, onFCP, onTTFB } from 'web-vitals';
 
-const reportWebVitals = (onPerfEntry?: ReportHandler) => {
-  if (onPerfEntry && onPerfEntry instanceof Function) {
-    import('web-vitals').then(({ getCLS, getFID, getFCP, getLCP, getTTFB }) => {
-      getCLS(onPerfEntry);
-      getFID(onPerfEntry);
-      getFCP(onPerfEntry);
-      getLCP(onPerfEntry);
-      getTTFB(onPerfEntry);
-    });
-  }
+const reportWebVitals = (onPerfEntry?: (metric: any) => void) => {
+    if (onPerfEntry && onPerfEntry instanceof Function) {
+        onCLS(onPerfEntry);
+        // onFID(onPerfEntry);
+        onLCP(onPerfEntry);
+        onFCP(onPerfEntry);
+        onTTFB(onPerfEntry);
+    }
 };
 
 export default reportWebVitals;
