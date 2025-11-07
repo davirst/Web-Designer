ddeafa918002dcc1fd9c50ff36fa6a0d839ced0f
David <ddebrah@gmail.com>
2025-11-07 14:47:45 -0700

    Vite configuration file

---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+    plugins: [
+        tailwindcss(),
+        react()
+    ],
+    server: {
+        // host: 'Nutritionpage.com',
+        port: 3000, // same as CRA default
+    },
+});
